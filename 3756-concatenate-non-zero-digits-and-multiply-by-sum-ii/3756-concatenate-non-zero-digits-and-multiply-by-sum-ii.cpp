84eb5821f22fb2abfffb25700ee5cf20c5b3a887
Sanjana Pal <sanjanapal004@gmail.com>
2026-07-08 23:45:10 +0530

    Time: 40 ms (61.66%), Space: 164.1 MB (32.13%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,63 +1,62 @@
 class Solution {
 public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-        vector<int>ans;
-        long long mod=1e9+7;
-
+        const long long MOD = 1000000007LL;
         int n = s.size();
-        //i precomputed the powers to help me in further extraction of the desired number from its prefix. i didnt use pow() function because its slow.
-        vector<long long> power(n + 1);
-        power[0] = 1;
-        for (int i=1;i<=n;i++) {
-        power[i] = (power[i-1]*10) % mod;
+
+        vector<int> nonZeroCount(n + 1, 0);
+
+        vector<int> digits;
+
+        for (int i = 0; i < n; i++) {
+            nonZeroCount[i + 1] = nonZeroCount[i];
+
+            if (s[i] != '0') {
+                nonZeroCount[i + 1]++;
+                digits.push_back(s[i] - '0');
+            }
         }
 
+        int k = digits.size();
 
-        //precomputed the numbers and their sums for each index.
-        vector<long long>prefix;
-        vector<pair<long long,int>>Numbers;
-        long long sum=0;
-        long long num=0;
-        int count=0;
-        for( int i=0;i<s.size();i++)
-        {
-            if(s[i] !='0')
-            {
-                num=((1LL*num*10)%mod+(s[i]-'0'))%mod;
-                count++;
-            }
-            sum+=(s[i]-'0')%mod;
-            prefix.push_back(sum);
-            Numbers.push_back({num,count});
+        vector<long long> prefixValue(k + 1, 0);
+
+        vector<long long> prefixSum(k + 1, 0);
+
+        vector<long long> power10(k + 1, 1);
+
+        for (int i = 0; i < k; i++) {
+            prefixValue[i + 1] = (prefixValue[i] * 10 + digits[i]) % MOD;
+
+            prefixSum[i + 1] = prefixSum[i] + digits[i];
+
+            power10[i + 1] = (power10[i] * 10) % MOD;
         }
-        for( int i=0; i<queries.size();i++)
-        {
-            int start=queries[i][0];
-            int end=queries[i][1];
-            int finalNum;
-            int requiredSum=0;
-            //if the number doesnt start from the very first index , it means it would require some cutting from its front.
 
+        vector<int> answer;
+        answer.reserve(queries.size());
 
-            if( start > 0 )
-            {
-                // this length is computed by the idea-- what was required length and what was its actual length since it takes all the number from the start.
-               int len =  Numbers[end].second - Numbers[start-1].second;
-               long long toMinus = (1LL * Numbers[start - 1].first * power[len]) % mod;
-               finalNum = (Numbers[end].first - toMinus + mod ) % mod;  
+        for (const auto& query : queries) {
+            int l = query[0];
+            int r = query[1];
 
-                int totalSum= prefix[end];
-                requiredSum = prefix[end] - prefix[start-1]; 
-            }
-            else
-            {
-                finalNum=Numbers[end].first;
-                requiredSum= prefix[end];
-            }
-            int val = (1LL*finalNum*requiredSum)%mod;
-            ans.push_back( val);
+            int left = nonZeroCount[l];
+
+            int right = nonZeroCount[r + 1];
+
+            int len = right - left;
+
+            long long x = (
+                prefixValue[right]
+                - (prefixValue[left] * power10[len]) % MOD
+                + MOD
+            ) % MOD;
+
+            long long sum = prefixSum[right] - prefixSum[left];
+
+            answer.push_back((int)((x * sum) % MOD));
         }
-        return ans;
-        
+
+        return answer;
     }
 };
